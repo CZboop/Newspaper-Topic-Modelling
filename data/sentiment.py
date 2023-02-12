8c1b1149afceb655c890f4c84e6df7b1c58c9e0d
Branchey <70140890+CZboop@users.noreply.github.com>
2023-02-12 10:04:51 +0000

    added logic to plot subjectivity of headlines

--- data/sentiment.py
+++ data/sentiment.py
@@ -8,9 +8,13 @@ import pandas as pd
 import plotly.express as px
 
 class SentimentAnalyser:
-    def __init__(self, data_processor):
+    def __init__(self, data_processor, source_name = None):
         self.data_processor = data_processor
         self.data_processor.read_and_concat_data_files()
+        if source_name != None:
+            self.source_name = source_name
+        else:
+            self.source_name = self.data_processor.selector.split('*')[0].title()
         # passing in a data processor object to handle reading data instead of repeating logic
         self.nlp = spacy.load('en_core_web_sm')
         self.nlp.add_pipe('spacytextblob')
@@ -32,14 +36,14 @@ class SentimentAnalyser:
         self.positive = self.data_df.loc[self.data_df['polarity'] > 0]
         self.negative = self.data_df.loc[self.data_df['polarity'] < 0]
         self.neutral = self.data_df.loc[self.data_df['polarity'] == 0]
-        print(positive)
+        
         positive_percent = len(self.positive) / total * 100
         negative_percent = len(self.negative) / total * 100
         neutral_percent = len(self.neutral) / total * 100
 
         return {'positive': positive_percent, 'negative': negative_percent, 'neutral': neutral_percent}
 
-    def plot_polarity_ratio(self, ratios, name): # ratio result of calling get_polarity_ratio, name of data source to use in title
+    def plot_polarity_ratio(self, ratios, name = self.source_name): # ratio result of calling get_polarity_ratio, name of data source to use in title
         perentage_df = DataFrame(ratios.items(), columns=['Polarity', 'Percentage'])
         fig = px.pie(percentage_df, values='Percentage', names='Polarity', title=f'{name} - Percentages of Positive, Negative and Neutral Headlines')
         self.save_as_json(fig, f'{name}_polarity_ratio')
@@ -50,7 +54,25 @@ class SentimentAnalyser:
     def plot_polarity_over_time(self):
         pass
 
-    def save_as_json(self, fig, name):
+    def get_subjectivity_info(self):
+        # get median, mean, but mostly will pass in to plotly to do a box plot 
+        if not hasattr(self, 'data_df'):
+            self._get_polarity_subjectivity()
+        median_subjectivity = self.data_df['subjectivity'].median()
+        mean_subjectivity = self.data_df['subjectivity'].mean()
+        return {'median': median_subjectivity, 'mean': mean_subjectivity}
+
+    def plot_subjectivity(self, name = self.source_name):
+        box_plot = px.box(self.data_df, y= 'subjectivity', title= f'{} - Subjectivity of Headlines')
+        self.save_as_json(box_plot, name)
+
+    def get_subjectivity_over_time(self):
+        pass 
+
+    def plot_subjectivity_over_time(self):
+        pass
+
+    def save_as_json(self, fig, name = self.source_name):
         fig.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)", legend_font_color="rgba(255,255,255,1)", title_font_color="rgba(255,255,255,1)", font=dict(color="rgba(255,255,255,1)"))
         # saving by getting relative path as absolute path
         path = Path(__file__).parent
@@ -58,4 +80,4 @@ class SentimentAnalyser:
 
 if __name__ == "__main__":
     sentiment_analyser = SentimentAnalyser(DataProcessor('../../uk_news_scraping/data', ['headline', 'date'], 'guardian*.csv'))
-    sentiment_analyser.get_polarity_ratio()
+    sentiment_analyser.plot_polarity_ratio(sentiment_analyser.get_polarity_ratio())
