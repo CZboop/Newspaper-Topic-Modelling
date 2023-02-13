ac12c022932e4d49c0c1a2621d95d3d0424d961f
Branchey <70140890+CZboop@users.noreply.github.com>
2023-02-13 21:29:01 +0000

    added helper class to run all polarity and subjectivity analysis and save as plots for all data sources

---
+++ data/multi_source_sentiments.py
@@ -0,0 +1,26 @@
+# a wee class to get all sentiment (polarity and subjectivity) info and graphs for sources
+from sentiment import SentimentAnalyser
+from data_processor import DataProcessor
+
+class SentimentGetter:
+    def __init__(self, selectors = {'guardian' : 'guardian_*.csv', 'mirror' : 'mirror_*.csv', 'telegraph': 'telegraph_*.csv', 
+    'sun' : 'sun_*.csv', 'mail' : 'mail_*.csv', 'metro' : 'metro_*.csv', 'express' : 'express_*.csv', 'all': '*.csv'}):
+        self.selectors = selectors
+
+    def run(self):
+        for name, selector in self.selectors.items():
+            self._source_sent(name, selector)
+
+    # running all analysis, plotting and saving for an individual data source
+    def _source_sent(self, name, selector):
+        sentiment_analyser = SentimentAnalyser(DataProcessor('../../uk_news_scraping/data', ['headline', 'date'], selector), name)
+        polarity_over_time = sentiment_analyser.get_polarity_over_time()
+        sentiment_analyser.plot_polarity_over_time(polarity_over_time)
+        sentiment_analyser.plot_polarity_ratio(sentiment_analyser.get_polarity_ratio())
+        subjectivity_over_time = sentiment_analyser.get_subjectivity_over_time()
+        sentiment_analyser.plot_subjectivity()
+        sentiment_analyser.plot_subjectivity_over_time(subjectivity_over_time)
+
+if __name__ == "__main__":
+    sentiment_getter = SentimentGetter()
+    sentiment_getter.run()
