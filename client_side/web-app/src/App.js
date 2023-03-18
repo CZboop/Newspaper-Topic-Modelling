eae87fc47850fd834a04ac8ecb61c39b53def47c
Branchey <70140890+CZboop@users.noreply.github.com>
2023-03-18 13:39:33 +0000

    added sun data plots and text content to the front end app

--- client_side/web-app/src/App.js
+++ client_side/web-app/src/App.js
@@ -12,6 +12,14 @@ import guardianSubjectivityPlot from './components/graph_data/guardian/guardian_
 import guardianSubjectivityTime from './components/graph_data/guardian/guardian_subjectivity_over_time.json';
 import guardianText from './components/text_data/guardian_text.json';
 
+import sunTopics from './components/graph_data/sun/sun_topics.json';
+import sunOverTime from './components/graph_data/sun/sun_over_time.json';
+import sunPolarityTime from './components/graph_data/sun/sun_polarity_over_time.json';
+import sunPolarityRatio from './components/graph_data/sun/sun_polarity_ratio.json';
+import sunSubjectivityPlot from './components/graph_data/sun/sun_subjectivity_box_plot.json';
+import sunSubjectivityTime from './components/graph_data/sun/sun_subjectivity_over_time.json';
+import sunText from './components/text_data/sun_text.json';
+
 function App() {
   return (
     <div className="App">
@@ -24,7 +32,7 @@ function App() {
           <Route path='metro' element={<NewspaperPage name='Metro' intro='lorem ipsum' extra_info='lorem ipsum' />}/>
           <Route path='mail' element={<NewspaperPage name='The Daily Mail' intro='lorem ipsum' extra_info='lorem ipsum'/>}/>
           <Route path='telegraph' element={<NewspaperPage name='The Telegraph' intro='lorem ipsum' extra_info='lorem ipsum'/>}/>
-          <Route path='sun' element={<NewspaperPage name='The Sun' intro='lorem ipsum' extra_info='lorem ipsum'/>}/>
+          <Route path='sun' element={<NewspaperPage name='The Sun' topic_intro={sunText.topic_intro} topic_plot={sunTopics} time_plot={sunOverTime} polarity_time={sunPolarityTime} polarity_ratio={sunPolarityRatio} subjectivity_box={sunSubjectivityPlot} subjectivity_over_time={sunSubjectivityTime} polarity_comments={sunText.polarity_comments} subjectivity_comments={sunText.subjectivity_comments} extra_info={sunText.extra_info}/>}/>
           <Route path='express' element={<NewspaperPage name='The Express' intro='lorem ipsum' extra_info='lorem ipsum'/>}/>
           </Routes>
         </BrowserRouter>
