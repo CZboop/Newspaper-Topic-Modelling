18fa2fb9ac18ff59d4b232a7cf767f7197dff122
Branchey <70140890+CZboop@users.noreply.github.com>
2023-03-28 07:58:27 +0100

    updated numbers and text in introduction page for web app to reflect filtering of data

--- client_side/web-app/src/components/Intro.js
+++ client_side/web-app/src/components/Intro.js
@@ -26,6 +26,8 @@ function Intro() {
         <p>The links above will take you to their respective websites, where you can read their reporting and support their work if you are interested.</p>
         <p>The percentages of articles from each news source can be seen below.</p>
         <Plot data={pieChart.data} layout={pieChart.layout}/>
+        <p>The total number of headlines analysed was around 3.05 million.</p>
+        <p>Ratios by source differed greatly, as can be seen above. The number of headlines for the biggest dataset (The Daily Mail) was around 1.89 million, and for the smallest dataset (The Guardian) was around 43,000.</p>
         <p>Headlines that form the data were published between November 2019 and early January 2023. The number of articles over time can be seen below.</p>
         {/* INSERT GRAPH HERE */}
         <Plot data={polarityOverTime.data} layout={polarityOverTime.layout}/>
@@ -41,9 +43,10 @@ function Intro() {
         <p>The line graph above shows the average (mean) subjectivity for each month across all news sources.</p>
         <p>We see that across the months, headlines averaged around 0.32 - more objective than not, but with a fair amount of subjectivity.</p>
         <p>Subjectivity was decreasing slightly over time until April 2021 when it began to increase over time slightly. However, the most notable trend was a sharp decline in subjectivity around November 2022.</p>
-        <p>The total number of headlines was around 3.85 million.</p>
         <h3>Data Limitations</h3>
-        <p>The data is not completely comparable across sources. One key difference is that for the Daily Mail, the Daily Express and the Metro, a complete or near-complete dataset of all article headlines was collected. On the other hand, for the other sources, headlines were collected from major news topics such as UK politics, world news, and health. These remaining sources should have a near-complete collection of those news topics but would exclude most coverage of areas such as showbiz, TV news, sports and other miscellaneous topics.</p>
+        <p>The data is not completely comparable across sources. One key difference is that for the Daily Mail, the Daily Express and the Metro, a complete or near-complete dataset of all article headlines was initially collected. For the Daily Mail, due to the very high number of articles, several topics were actively filtered out before analysis - 'wires' (these were republications of articles from other sources such as Reuters and AP), as well as sport, showbiz and 'femail' (lifestyle articles targeted at women). This was done based on the topics as classified by the newspaper itself.</p>
+        <p>The number and ratio of headlines above is after filtering, so reflects the headlines that were analysed rather than the starting dataset.</p>
+        <p>On the other hand, for the other sources, headlines were collected from major news topics such as UK politics, world news, and health. These remaining sources should have a near-complete collection of those news topics but would exclude most coverage of areas such as showbiz, TV news, sports and other miscellaneous topics.</p>
 
     </div>
   )
