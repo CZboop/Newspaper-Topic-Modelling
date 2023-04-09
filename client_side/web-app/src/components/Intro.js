a0e53c0caacb33e5532bb2e3642ec1bc3e2407c2
Branchey <70140890+CZboop@users.noreply.github.com>
2023-04-09 10:07:17 +0100

    added different classes for different types of text in web app, added svg info icon and started on some styling

--- client_side/web-app/src/components/Intro.js
+++ client_side/web-app/src/components/Intro.js
@@ -12,6 +12,9 @@ import subjectivityOverTime from './graph_data/combined/all_subjectivity_over_ti
 import articlesOverTimeAll from './graph_data/combined/articles_over_time_All Sources.json';
 import articlesOverTimeCombined from './graph_data/combined/articles_over_time_Combined Sources.json';
 
+// importing image
+import InfoIcon from './InfoIcon.js';
+
 function Intro() {
   const { width, height, ref } = useResizeDetector({ 
     refreshMode: 'debounce', 
@@ -44,6 +47,7 @@ function Intro() {
   return (
     <div className='Intro page-content'>
         <h2>Introduction</h2>
+        <div className="comment text-module">
         <h3>The Shape of the Data</h3>
         <p>Headline data was gathered from seven of the top newspapers in the UK:</p>
         <ul>
@@ -56,6 +60,8 @@ function Intro() {
             <li className='highlighted-link'><a href='https://www.express.co.uk/'>The Daily Express</a></li>
         </ul>
         <p>The links above will take you to their respective websites, where you can read their reporting and support their work if you are interested.</p>
+        </div>
+        <div className="comment text-module">
         <p>The percentages of articles from each news source can be seen below. You can click on the sources in the legend to remove or add them from the chart, for example to better compare document numbers between certain sources.</p>
         {
         // different legend position so chart more vertical if small screen as can squeeze the chart too small otherwise on smaller screens
@@ -70,7 +76,10 @@ function Intro() {
         }
         <p>The total number of headlines analysed was around 3.05 million.</p>
         <p>Ratios by source differed greatly, as can be seen above. The number of headlines for the biggest dataset (The Daily Mail) was around 1.89 million, and for the smallest dataset (The Guardian) was around 43,000.</p>
+        </div>
+        <div className="comment text-module">
         <p>Headlines that form the data were published between November 2019 and early January 2023. The number of articles over time can be seen below. The dip in the last month is most likely due to the fact that data was incomplete for this month - only from the start of the month not the whole of it.</p>
+        </div>
         {
         // different legend position for small screens more vertical
         width <= 600 ?
@@ -82,17 +91,29 @@ function Intro() {
         <Plot data={articlesOverTimeAll.data} layout={{...articlesOverTimeAll.layout, ...{title: {text: titles["articlesOverTimeAll"], font: {color: "white"}}, width: width, height: height, legend:{font:{size: '2%', color: "white"}}}}} config = {{responsive: true}}/>
         </div>
         }
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>Click on a source name from the key to show or hide the line for that newspaper. Double-click to isolate one news source.</p>
+        </div>
+        <div className="comment text-module">
         <p>The Daily Mail had quite similar numbers of articles over time. The Sun and the Metro showed a trend of generally less articles over time. The Telegraph also had less articles over time, but this seemed to be in two stages rather than an overall trend - in mid-2021 there was a dip in article numbers and they stayed similarly low since then. The Guardian showed a slight increase in average article numbers over time, as did the Express. The Mirror showed the biggest change in articles each month, with a very clear trend of increasing articles over time, especially since 2021. For the Mirror, average articles were around 3000 per month, rising to over double this at the end of 2022.</p>
+        </div>
         <div ref={ref} class="graph-container">
         <Plot data={articlesOverTimeCombined.data} layout={{...articlesOverTimeCombined.layout, ...{title: {text: titles["articlesOverTimeCombined"], font: {color: "white"}}, width: width, height: height, legend:{font:{size: '2%', color: "white"}}}}} config = {{responsive: true}}/>
         </div>
+        <div className="comment text-module">
         <p>As the graph above shows, the number of articles over time was relatively stable overall, with a significant dip at the end due to only part of the month being in the dataset. However, this is likely due to the disproportionate influence of the larger Daily Mail dataset, which ends up being very similar in shape to the combined data.</p>
+        </div>
         <div ref={ref} class="graph-container">
         <Plot data={polarityOverTime.data} layout={{...polarityOverTime.layout, ...{title: {text: titles["polarityOverTime"], font: {color: "white"}}, width: width, height: height, legend:{font:{size: '2%', color: "white"}}}}} config = {{responsive: true}}/>
         </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>Polarity is a measure of how positive or negative the language used in a text or set of texts is. Above is a graph showing the average polarity across all headlines from all news sources for each month. Polarity in this case goes from a maximum of 1 (very positive) to -1 (very negative).</p>
+        </div>
+        <div className="comment text-module">
         <p>Headlines tended to be neutral to slightly positive on average across large samples. Considering the possible range, polarity was fairly stable across the years in the data. However, there are two notable dips where headlines became more negative - the end of 2020 into the start of 2021, and the end of 2022 into the start of 2023.</p>
+        </div>
         {
         // different legend position for small screens more vertical
         width <= 600 ?
@@ -104,26 +125,37 @@ function Intro() {
         <Plot data={polarityRatio.data} layout={{...polarityRatio.layout, ...{title: {text: titles["polarityRatio"], font: {color: "white"}},width: width, height: height, legend:{font:{size: '2%', color: "white"}}}}} config = {{responsive: true}}/>
         </div>
         }
+        <div className="comment text-module">
         <p>The graph above shows the ratio of all headlines that were mostly positive, mostly negative or neutral. Here, a headline is only neutral if it scored 0 on polarity. The plurality of headlines were still neutral (43%), positive headlines were next most common at 34%, and the remaining 23% of headline were negative.</p>
+        </div>
         <div ref={ref} class="graph-container">
         <Plot data={subjectivityPlot.data} layout={{...subjectivityPlot.layout, ...{title: {text: titles["subjectivityPlot"], font: {color: "white"}}, width: width, height: height, legend:{font:{size: '2%', color: "white"}}}}} config = {{responsive: true}}/>
         </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>Subjectivity represents how objective or subjective language used is, ranging from 0 (maximally objective) to 1 (maximally subjective).</p>
+        </div>
+        <div className="comment text-module">
         <p>The box plot above shows that the data included headlines that were at both limits of subjectivity, but most headlines were more objective, including a significant number (at least a quarter) that were completely objective based on this way of measuring linguistic objectivity.</p>
         <p>Median subjectivity was around 0.29, and around a quarter of all headlines were more subjective than objective.</p>
+        </div>
         <div ref={ref} class="graph-container">
         <Plot data={subjectivityOverTime.data} layout={{...subjectivityOverTime.layout, ...{title: {text: titles["subjectivityOverTime"], font: {color: "white"}},width: width, height: height, legend:{font:{size: '2%', color: "white"}}}}} config = {{responsive: true}}/>
         </div>
+        <div className="comment text-module">
         <p>The line graph above shows the average (mean) subjectivity for each month across all news sources.</p>
         <p>We see that across the months, headlines averaged around 0.32 - more objective than not, but with a fair amount of subjectivity.</p>
         <p>Subjectivity was decreasing slightly over time until April 2021 when it began to increase over time slightly. However, the most notable trend was a sharp decline in subjectivity around November 2022.</p>
+        </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <h3>Data Limitations</h3>
         <p>The data is not completely comparable across sources. One key difference is that for the Daily Mail, the Daily Express and the Metro, a complete or near-complete dataset of all article headlines was initially collected. For the Daily Mail, due to the very high number of articles, several topics were actively filtered out before analysis - 'wires' (these were republications of articles from other sources such as Reuters and AP), as well as sport, showbiz and 'femail' (lifestyle articles targeted at women). This was done based on the topics as classified by the newspaper itself, and had the effect of removing around 800,000 headlines.</p>
         <p>The number and ratio of headlines above is after filtering, so reflects the headlines that were analysed rather than the starting dataset.</p>
         <p>On the other hand, for the other sources, headlines were collected from major news topics such as UK politics, world news, and health. These remaining sources should have a near-complete collection of those news topics but would exclude most coverage of areas such as showbiz, TV news, sports and other miscellaneous topics.</p>
-
+        </div>
     </div>
   )
 }
 
-export default Intro
+export default Intro;
--- client_side/web-app/src/components/NewspaperPage.js
+++ client_side/web-app/src/components/NewspaperPage.js
@@ -4,6 +4,9 @@ import { useResizeDetector } from 'react-resize-detector';
 import './NewspaperPage.css';
 import { useState, useCallback, useEffect } from "react";
 
+// importing image - svg icon as component
+import InfoIcon from './InfoIcon.js';
+
 function NewspaperPage({name, topic_intro, topic_plot, time_plot, polarity_time, polarity_ratio, subjectivity_box, subjectivity_over_time, extra_info, polarity_comments, subjectivity_comments}) {
   // using resize detector npm module to get height and width to resize graphs with window size
   // using debounce so updates once per second at most, no glitching
@@ -44,18 +47,26 @@ function NewspaperPage({name, topic_intro, topic_plot, time_plot, polarity_time,
   return (
     <div className='newspaper-container page-content'>
         <h2>{name}</h2>
+        <div className="comment text-module">
         <h3>Topics</h3>
         <p>{topic_intro}</p>
+        </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>An interactive plot of the topics can be seen below.</p>
         <p>You can mouse over each topic to see the summary of the words that define it, as well as its number which corresponds to how many headlines form the cluster. Topic 0 will be the biggest group with most articles.</p>
         <p>Hovering over each topic will also show a representative example of a headline from the group. These differ from the original headline in that stopwords and capitalisation have been removed.</p>
         <p>There are pan and zoom options available from the top right of the plot, and the slider along the bottom cycles through the topics one by one, highlighting each one on the graph.</p>
+        </div>
         <div ref={ref} class="graph-container">
         {/* using spread operator so adding to layout with resonsive height width in one line keeping it as single object */}
         <Plot data={topic_plot.data} layout={{...topic_plot.layout, ...{width: width, height: height, legend:{font:{size: '2%'}}}, title: {text: titles["topic_plot"]}}} config = {{responsive: true}}/>
         </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>An interactive plot of topics over time can be found below.</p>
         <p>You can select or deselect different topics to show, hide or isolate them for clarity.</p>
+        </div>
         {
         // some of the graphs have legend overlap or hard to read on smaller screens, using legend settings below to prevent this if width less than 800
         width <= 800 ?
@@ -67,9 +78,12 @@ function NewspaperPage({name, topic_intro, topic_plot, time_plot, polarity_time,
         <Plot data={time_plot.data} layout={{...time_plot.layout, ...{width: width}, title: {text: titles["time_plot"]}}}/>
         </div>
         }
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <h3>Polarity</h3>
         <p>Polarity is a measure of how positive or negative language is. In this instance, this goes from -1 which is the most negative, or 1 which is the most positive. 0 is completely neutral in terms of polarity.</p>
         <p>The pie chart below shows the ratio of headlines which are positive, negative, or completely neutral according to the polarity analysis run.</p>
+        </div>
         {
         // again setting legend position based on screen size when the graph looks off because of legend
         width <= 600 ?
@@ -81,23 +95,36 @@ function NewspaperPage({name, topic_intro, topic_plot, time_plot, polarity_time,
         <Plot data={polarity_ratio.data} layout={{...polarity_ratio.layout, ...{width: width, height: height, title: {text: titles["polarity_ratio"]}}}}/>
         </div>
         }
-        <p>The line graph below shows the polarity over time, where the polarity was averaged for each month.</p>        
+        <div className="boilerplate text-module">
+        <InfoIcon />
+        <p>The line graph below shows the polarity over time, where the polarity was averaged for each month.</p>    
+        </div>    
         <div ref={ref} class="graph-container">
         <Plot data={polarity_time.data} layout={{...polarity_time.layout, ...{width: width, height: height, title: {text: titles["polarity_time"]}}}}/>
         </div>
-        
+        <div className="comment text-module">
         <p>{polarity_comments}</p>
+        </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <h3>Subjectivity</h3>
         <p>Subjectivity is a measure of subjective (opinionated) or objective (factual) language is. In this instance, this goes from 0 which is the most objective, to 1 which is the most subjective. We could think of 0.5 as an equal mix of fact and opinion.</p>
         <p>The box plot below shows the minimum and maximum subjectivity for this news source, as well as the quartiles.</p>
+        </div>
         <div ref={ref} class="graph-container">
         <Plot data={subjectivity_box.data} layout={{...subjectivity_box.layout, ...{width: width, height: height, title: {text: titles["subjectivity_box"]}}}}/>
         </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>The line graph below shows the subjectivity over time, where the subjectivity was averaged for each month.</p>
+        </div>
         <div ref={ref} class="graph-container">
         <Plot data={subjectivity_over_time.data} layout={{...subjectivity_over_time.layout, ...{width: width, height: height, title: {text: titles["subjectivity_over_time"]}}}}/>
         </div>
+        <div className="boilerplate text-module">
+        <InfoIcon />
         <p>{subjectivity_comments}</p>
+        </div>
         <p className='extra-info'>{extra_info}</p>
         {/* TODO: potentially hide the extra info if it's falsy or remove if not used in any anyway*/}
 
