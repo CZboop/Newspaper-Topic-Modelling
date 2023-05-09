a45340673ba55976985bb110cfa9244952658ce7
Branchey <70140890+CZboop@users.noreply.github.com>
2023-05-09 07:57:46 +0100

    added key prop to elements within main newspaper page component in react app, to prevent issue where route change was not consistently causing state change

--- client_side/web-app/src/components/NewspaperPage.js
+++ client_side/web-app/src/components/NewspaperPage.js
@@ -51,51 +51,51 @@ function NewspaperPage({name, topic_intro, topic_plot, time_plot, polarity_time,
 
   return (
     <div className='newspaper-container page-content'>
-      <TextWindow title={name} textArray={[""]} pageTitle={true}/>
-      <TextWindow title={"Topics"} textArray={[topic_intro]} pageTitle={false}/>
-      <TextInfo title={null} textArray= {["An interactive plot of the topics can be seen below.,You can mouse over each topic to see the summary of the words that define it, as well as its number which corresponds to how many headlines form the cluster. Topic 0 will be the biggest group with most articles.","Hovering over each topic will also show a representative example of a headline from the group. These differ from the original headline in that stopwords and capitalisation have been removed.","There are pan and zoom options available from the top right of the plot, and the slider along the bottom cycles through the topics one by one, highlighting each one on the graph."]}/>
-      <div ref={ref} className="graph-container">
+      <TextWindow title={name} textArray={[""]} pageTitle={true} key={`${name}Title`}/>
+      <TextWindow title={"Topics"} textArray={[topic_intro]} pageTitle={false} key={`${name}TopicsIntro`}/>
+      <TextInfo title={null} textArray= {["An interactive plot of the topics can be seen below.,You can mouse over each topic to see the summary of the words that define it, as well as its number which corresponds to how many headlines form the cluster. Topic 0 will be the biggest group with most articles.","Hovering over each topic will also show a representative example of a headline from the group. These differ from the original headline in that stopwords and capitalisation have been removed.","There are pan and zoom options available from the top right of the plot, and the slider along the bottom cycles through the topics one by one, highlighting each one on the graph."]} key={`${name}TopicInfo`}/>
+      <div ref={ref} className="graph-container" key={`${name}TopicGraph`}>
       {/* using spread operator so adding to layout with resonsive height width in one line keeping it as single object */}
-      <Plot data={topic_plot.data} layout={{...topic_plot.layout, ...{width: width, height: height, legend:{font:{size: '2%'}}}, title: {text: titles["topic_plot"]}}} config = {{responsive: true}}/>
+      <Plot data={topic_plot.data} layout={{...topic_plot.layout, ...{width: width, height: height, legend:{font:{size: '2%'}}}, title: {text: titles["topic_plot"]}}} config = {{responsive: true}} key={`${name}PlotlyPlot`}/>
       </div>
-      <TextInfo title={null} textArray={["An interactive plot of topics over time can be found below.", "You can select or deselect different topics to show, hide or isolate them for clarity."]} />
+      <TextInfo title={null} textArray={["An interactive plot of topics over time can be found below.", "You can select or deselect different topics to show, hide or isolate them for clarity."]} key={`${name}OverTimeInfo`} />
         {
         // some of the graphs have legend overlap or hard to read on smaller screens, using legend settings below to prevent this if width less than 800
         width <= 800 ?
-        <div ref={ref} className="graph-container">
-        <Plot data={time_plot.data} layout={{...time_plot.layout, ...{width: width, showlegend: true, legend: {x: 0, y:-1.8, xanchor:"left", yanchor:"bottom"}, title: {text: titles["time_plot"]}}}}/>
+        <div ref={ref} className="graph-container" key={`${name}OverTimeContainer`} >
+        <Plot data={time_plot.data} layout={{...time_plot.layout, ...{width: width, showlegend: true, legend: {x: 0, y:-1.8, xanchor:"left", yanchor:"bottom"}, title: {text: titles["time_plot"]}}}} key={`${name}OverTimePlotlyPlot`}/>
         </div>
         :
-        <div ref={ref} className="graph-container">
-        <Plot data={time_plot.data} layout={{...time_plot.layout, ...{width: width}, title: {text: titles["time_plot"]}}}/>
+        <div ref={ref} className="graph-container" key={`${name}OverTimeContainer`} >
+        <Plot data={time_plot.data} layout={{...time_plot.layout, ...{width: width}, title: {text: titles["time_plot"]}}} key={`${name}OverTimePlotlyPlot`}/>
         </div>
         }
-        <TextInfo title={"Polarity"} textArray={["Polarity is a measure of how positive or negative language is. In this instance, this goes from -1 which is the most negative, or 1 which is the most positive. 0 is completely neutral in terms of polarity.", "The pie chart below shows the ratio of headlines which are positive, negative, or completely neutral according to the polarity analysis run."]}/>
+        <TextInfo title={"Polarity"} textArray={["Polarity is a measure of how positive or negative language is. In this instance, this goes from -1 which is the most negative, or 1 which is the most positive. 0 is completely neutral in terms of polarity.", "The pie chart below shows the ratio of headlines which are positive, negative, or completely neutral according to the polarity analysis run."]} key={`${name}PolarityInfo`}/>
         {
         // again setting legend position based on screen size when the graph looks off because of legend
         width <= 600 ?
-        <div ref={ref} className="graph-container">
-        <Plot data={polarity_ratio.data} layout={{...polarity_ratio.layout, ...{width: width, showlegend: true, legend: {x: 0, y:-0.5, xanchor:"left", yanchor:"bottom"}, title: {text: titles["polarity_ratio"]}}}}/>
+        <div ref={ref} className="graph-container" key={`${name}PolarityRatioGraphContainer`} >
+        <Plot data={polarity_ratio.data} layout={{...polarity_ratio.layout, ...{width: width, showlegend: true, legend: {x: 0, y:-0.5, xanchor:"left", yanchor:"bottom"}, title: {text: titles["polarity_ratio"]}}}} key={`${name}PolarityRatioPlotlyPlot`} />
         </div>
         :
-        <div ref={ref} className="graph-container">
-        <Plot data={polarity_ratio.data} layout={{...polarity_ratio.layout, ...{width: width, height: height, title: {text: titles["polarity_ratio"]}}}}/>
+        <div ref={ref} className="graph-container" key={`${name}PolarityRatioGraphContainer`} >
+        <Plot data={polarity_ratio.data} layout={{...polarity_ratio.layout, ...{width: width, height: height, title: {text: titles["polarity_ratio"]}}}} key={`${name}PolarityRatioPlotlyPlot`}/>
         </div>
         }
-        <TextInfo title={null} textArray={["The line graph below shows the polarity over time, where the polarity was averaged for each month."]}/>
-        <div ref={ref} className="graph-container">
-        <Plot data={polarity_time.data} layout={{...polarity_time.layout, ...{width: width, height: height, title: {text: titles["polarity_time"]}}}}/>
+        <TextInfo title={null} textArray={["The line graph below shows the polarity over time, where the polarity was averaged for each month."]} key={`${name}PolarityOverTimeInfo`}/>
+        <div ref={ref} className="graph-container" key={`${name}PolarityOverTimeGraphContainer`}>
+        <Plot data={polarity_time.data} layout={{...polarity_time.layout, ...{width: width, height: height, title: {text: titles["polarity_time"]}}}} key={`${name}PolarityOverTimePlot`}/>
         </div>
-        <TextWindow title={`${shortName} - Polarity`} textArray={[polarity_comments]} pageTitle={false}/>
-        <TextInfo title={"Subjectivity"} textArray={["Subjectivity is a measure of subjective (opinionated) or objective (factual) language is. In this instance, this goes from 0 which is the most objective, to 1 which is the most subjective. We could think of 0.5 as an equal mix of fact and opinion.", "The box plot below shows the minimum and maximum subjectivity for this news source, as well as the quartiles."]}/>
-        <div ref={ref} className="graph-container">
-        <Plot data={subjectivity_box.data} layout={{...subjectivity_box.layout, ...{width: width, height: height, title: {text: titles["subjectivity_box"]}}}}/>
+        <TextWindow title={`${shortName} - Polarity`} textArray={[polarity_comments]} pageTitle={false} key={`${name}PolarityComments`}/>
+        <TextInfo title={"Subjectivity"} textArray={["Subjectivity is a measure of subjective (opinionated) or objective (factual) language is. In this instance, this goes from 0 which is the most objective, to 1 which is the most subjective. We could think of 0.5 as an equal mix of fact and opinion.", "The box plot below shows the minimum and maximum subjectivity for this news source, as well as the quartiles."]} key={`${name}SubjectivityInfo`}/>
+        <div ref={ref} className="graph-container" key={`${name}SubjectivityBoxPlotContainer`}>
+        <Plot data={subjectivity_box.data} layout={{...subjectivity_box.layout, ...{width: width, height: height, title: {text: titles["subjectivity_box"]}}}} key={`${name}SubjectivityBoxPlotlyPlot`} />
         </div>
-        <TextInfo title={null} textArray={["The line graph below shows the subjectivity over time, where the subjectivity was averaged for each month."]}/>
-        <div ref={ref} className="graph-container">
-        <Plot data={subjectivity_over_time.data} layout={{...subjectivity_over_time.layout, ...{width: width, height: height, title: {text: titles["subjectivity_over_time"]}}}}/>
+        <TextInfo title={null} textArray={["The line graph below shows the subjectivity over time, where the subjectivity was averaged for each month."]} key={`${name}SubjectivityOverTimeInfo`}/>
+        <div ref={ref} className="graph-container" key={`${name}SubjectivityOverTimePlotContainer`}>
+        <Plot data={subjectivity_over_time.data} layout={{...subjectivity_over_time.layout, ...{width: width, height: height, title: {text: titles["subjectivity_over_time"]}}}} key={`${name}SubjectivityBoxPlotlyPlot`}/>
         </div>
-        <TextWindow title={`${shortName} - Subjectivity`} textArray={[subjectivity_comments]} pageTitle={false}/>
+        <TextWindow title={`${shortName} - Subjectivity`} textArray={[subjectivity_comments]} pageTitle={false} key={`${name}SubjectivityComments`}/>
     </div>
   )
 }
